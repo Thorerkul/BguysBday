9c062b49c511a2ff086db198459d42a04b1cdc6c
T - TA <thoukkol@gmail.com>
2023-09-19 17:47:08 +0200

    gfd

--- My project/Assets/Scripts/enemyScript.cs
+++ My project/Assets/Scripts/enemyScript.cs
@@ -6,8 +6,16 @@ public class enemyScript : MonoBehaviour
 {
     public EntityBase basis;
     public AttackTypes currentAttack;
+    public SpriteRenderer enemyspr;
 
     public Animator anim;
+
+    public Color enemyColor;
+
+    private void Update()
+    {
+        enemyspr.color = enemyColor;
+    }
 
     public void attack(AttackTypes type, playerScript enemy, float damage)
     {
